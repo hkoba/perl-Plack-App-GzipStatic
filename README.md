e0b81a7fa61131671d2edf486f8c9d001b60b958
Kobayasi, Hiroaki <buribullet@gmail.com>
2017-01-07 13:01:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::App::GzipStatic - It's new $module
+
+# SYNOPSIS
+
+    use Plack::App::GzipStatic;
+
+# DESCRIPTION
+
+Plack::App::GzipStatic is ...
+
+# LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kobayasi, Hiroaki <buribullet@gmail.com>
---
+++ lib/Plack/App/GzipStatic.pm
@@ -0,0 +1,39 @@
+package Plack::App::GzipStatic;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::App::GzipStatic - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::App::GzipStatic;
+
+=head1 DESCRIPTION
+
+Plack::App::GzipStatic is ...
+
+=head1 LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kobayasi, Hiroaki E<lt>buribullet@gmail.comE<gt>
+
+=cut
+
